27754563a407b00576e5fb4a1b36cf0d81a60c33
Abdelrahman Moez <abdelrahman.moez@gmail.com>
2013-11-23 20:06:49 +0200

    public IP
    This snippet is to get your public ip address.

---
+++ public_ip.py
@@ -0,0 +1,15 @@
+# import needed modules
+import re
+import urllib2
+
+def public_ip():
+    # setting the website to use to get the public ip
+    address = "http://www.ipchicken.com"
+    # grabbing source code of the page
+    src_code = urllib2.urlopen(address).read()
+    # parsing public ip
+    public_ip = re.search(r'\d+\.\d+\.\d+\.\d+', src_code).group()
+    return public_ip
+
+# layout public ip
+print public_ip()
